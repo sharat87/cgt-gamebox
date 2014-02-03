54140b534143c911046d0928b18a0e45b3cafde3
Shrikant Sharat <shrikantsharat.k@gmail.com>
2014-02-03 12:28:36 +0530

    Moved game logic to separate service.

--- main.js
+++ main.js
@@ -1,16 +1,93 @@
 angular.module('gamebox', [])
 
-.controller('MainCtrl', function ($scope) {
-    $scope.width = 800;
-    $scope.height = 400;
+.run(function ($rootScope) {
+    $rootScope.turn = 'red';
+})
 
-    $scope.turn = 'red';
+.factory('Hackenbush', function ($rootScope) {
 
-    $scope.$watch('height', function (height) {
-        $scope.floorY = height - 20;
-    });
+    function Hackenbush(width, height, lines) {
+        this.lines = lines;
+        this.width = width;
+        this.height = height;
+        this.floorY = height - 20;
+        this.cacheVertices();
+    }
 
-    $scope.bush = [
+    Hackenbush.prototype = {
+
+        cacheVertices: function () {
+            var verts = [], sverts = [];
+            for (var i = this.lines.length; i-- > 0;) {
+                var line = this.lines[i];
+                var point = [line.x1, line.y1].join();
+                if (sverts.indexOf(point) < 0) {
+                    sverts.push(point);
+                    verts.push([line.x1, line.y1]);
+                }
+                point = [line.x2, line.y2].join();
+                if (sverts.indexOf(point) < 0) {
+                    sverts.push(point);
+                    verts.push([line.x2, line.y2]);
+                }
+            }
+            this.verts = verts;
+        },
+
+        strike: function (line) {
+            line.gone = true;
+            this.strikeFloating();
+            $rootScope.turn = $rootScope.turn == 'red' ? 'blue' : 'red';
+            this.checkWin();
+        },
+
+        strikeFloating: function () {
+            var heldPoints = [], traversalDone = false;
+            while (true) {
+                var prevHeld = heldPoints.length;
+                for (var i = this.lines.length; i-- > 0;) {
+                    var line = this.lines[i];
+                    var a = [line.x1, line.y1].join(), b = [line.x2, line.y2].join();
+                    if (line.gone) {
+                        continue;
+                    } else if (line.y1 == this.floorY || line.y2 == this.floorY) {
+                        if (heldPoints.indexOf(a) < 0)
+                            heldPoints.push(a);
+                        if (heldPoints.indexOf(b) < 0)
+                            heldPoints.push(b);
+                    } else if (heldPoints.indexOf(a) >= 0) {
+                        if (heldPoints.indexOf(b) < 0)
+                            heldPoints.push(b);
+                    } else if (heldPoints.indexOf(b) >= 0) {
+                        if (heldPoints.indexOf(a) < 0)
+                            heldPoints.push(a);
+                    } else if (traversalDone) {
+                        line.gone = true;
+                    }
+                }
+                if (traversalDone)
+                    break;
+                if (prevHeld == heldPoints.length)
+                    traversalDone = true;
+            }
+        },
+
+        checkWin: function () {
+            for (var i = this.lines.length; i-- > 0;) {
+                var line = this.lines[i];
+                if (!line.gone && (line.color == $rootScope.turn || line.color == 'green'))
+                    return;
+            }
+            $scope.winner = $scope.turn == 'red' ? 'blue' : 'red';
+        }
+
+    };
+
+    return Hackenbush;
+})
+
+.controller('MainCtrl', function ($scope, Hackenbush) {
+    $scope.game = new Hackenbush(800, 400, [
         {x1: 150, y1: 380, x2: 200, y2: 280, color: 'red'},
         {x1: 250, y1: 380, x2: 200, y2: 280, color: 'red'},
         {x1: 150, y1: 220, x2: 200, y2: 280, color: 'blue'},
@@ -19,71 +96,5 @@ angular.module('gamebox', [])
         {x1: 550, y1: 380, x2: 500, y2: 280, color: 'blue'},
         {x1: 600, y1: 170, x2: 500, y2: 280, color: 'red'},
         {x1: 600, y1: 170, x2: 650, y2: 200, color: 'green'}
-    ];
-
-    $scope.$watch('bush', function () {
-        var points = [], spoints = [];
-        for (var i = $scope.bush.length; i-- > 0;) {
-            var line = $scope.bush[i];
-            var point = [line.x1, line.y1].join();
-            if (spoints.indexOf(point) < 0) {
-                spoints.push(point);
-                points.push([line.x1, line.y1]);
-            }
-            point = [line.x2, line.y2].join();
-            if (spoints.indexOf(point) < 0) {
-                spoints.push(point);
-                points.push([line.x2, line.y2]);
-            }
-        }
-        $scope.points = points;
-    }, true);
-
-    $scope.strike = function (line) {
-        line.striked = true;
-        line.gone = true;
-        floatDisconnected();
-        $scope.turn = $scope.turn == 'red' ? 'blue' : 'red';
-        checkWin();
-    };
-
-    function floatDisconnected() {
-        var heldPoints = [], traversalDone = false;
-        while (true) {
-            var prevHeld = heldPoints.length;
-            for (var i = $scope.bush.length; i-- > 0;) {
-                var line = $scope.bush[i];
-                var a = [line.x1, line.y1].join(), b = [line.x2, line.y2].join();
-                if (line.gone) {
-                    continue;
-                } else if (line.y1 == $scope.floorY || line.y2 == $scope.floorY) {
-                    if (heldPoints.indexOf(a) < 0)
-                        heldPoints.push(a);
-                    if (heldPoints.indexOf(b) < 0)
-                        heldPoints.push(b);
-                } else if (heldPoints.indexOf(a) >= 0) {
-                    if (heldPoints.indexOf(b) < 0)
-                        heldPoints.push(b);
-                } else if (heldPoints.indexOf(b) >= 0) {
-                    if (heldPoints.indexOf(a) < 0)
-                        heldPoints.push(a);
-                } else if (traversalDone) {
-                    line.gone = true;
-                }
-            }
-            if (traversalDone)
-                break;
-            if (prevHeld == heldPoints.length)
-                traversalDone = true;
-        }
-    }
-
-    function checkWin() {
-        for (var i = $scope.bush.length; i-- > 0;) {
-            var line = $scope.bush[i];
-            if (!line.gone && (line.color == $scope.turn || line.color == 'green'))
-                return;
-        }
-        $scope.winner = $scope.turn == 'red' ? 'blue' : 'red';
-    }
+    ]);
 });
